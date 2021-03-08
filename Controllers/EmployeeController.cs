fb770c92d73da648822e7b3f9a78040dbb3ad278
Ayub Metah <admin@ayubtechnologies.co.ke>
2021-03-08 15:32:19 +0300

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Employee_Portal.Models;
+
+namespace Employee_Portal.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Repository.AllEmployees);
+        }
+
+        // HTTP GET VERSION
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // HTTP POST VERSION  
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Create(employee);
+                return View("Thanks", employee);
+            }
+            else
+                return View();
+        }
+
+        public IActionResult Update(string empname)
+        {
+            Employee employee = Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee employee, string empname)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Name = employee.Name;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Nationality = employee.Nationality;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().DOB = employee.DOB;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Age = employee.Age;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Passport = employee.Passport;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Name = employee.Name;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().QID = employee.QID;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Gender = employee.Gender;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().MaritalStatus = employee.MaritalStatus;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().JoiningDate = employee.JoiningDate;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Salary = employee.Salary;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Department = employee.Department;
+                Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault().Education = employee.Education;
+
+                               
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string empname)
+        {
+            Employee employee = Repository.AllEmployees.Where(e => e.Name == empname).FirstOrDefault();
+            Repository.Delete(employee);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Portal.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your nationality")]
+        public string Nationality { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)] 
+        public DateTime DOB { get; set; }
+
+        [RegularExpression(@"\d+(\.\d{1,2})?", ErrorMessage = "Invalid input, only integers accepted")]
+        [Range(16, 60, ErrorMessage = "Age is out of range, 20-60 only")]
+        public int Age { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your Passport number")]
+        public string Passport { get; set; }
+
+        [Column(TypeName = "int(11")]
+        public decimal QID { get; set; }
+
+        [RegularExpression(@"^[MF]+$", ErrorMessage = "Select any one option")]
+        public Char Gender { get; set; }
+
+        [RegularExpression(@"^[MF]+$", ErrorMessage = "Select any one option")]
+        public Char MaritalStatus { get; set; }
+
+        [Display(Name = "Joining Date")]
+        [DataType(DataType.Date)]
+        public DateTime JoiningDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 0)")]
+        public decimal Salary { get; set; }
+
+        public string Department { get; set; }
+
+        [Required(ErrorMessage = "Please enter your academic qualification")]
+        public string Education { get; set; }
+
+    }
+}
